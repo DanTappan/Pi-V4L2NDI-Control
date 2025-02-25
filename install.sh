45f10d0b5dbb1485ece22b6ae18c96dbb71ed70f
DanTappan <Dan.Tappan@gmail.com>
2025-02-25 10:30:23 -0500

    Update install.sh
    switch back to lplassman repository

--- install.sh
+++ install.sh
@@ -10,10 +10,9 @@ sudo apt update -y && sudo apt upgrade -y
 sudo apt-get install v4l-utils
 
 # Run lplassman script to install v4l2ndi
-# for now, use my fork of the code
 (
 sudo rm -f /usr/bin/v4l2ndi
-git clone https://github.com/DanTappan/V4L2-to-NDI.git
+git clone https://github.com/lplassman/V4L2-to-NDI.git
 cd V4L2-to-NDI
 sudo bash ./easy-install-rpi-aarch64.sh
 )
